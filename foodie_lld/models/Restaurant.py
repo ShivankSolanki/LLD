9f71c09f2637fb98db19eaf8fe4f0cf1b28efb41
Shivank <shivank1972@gmail.com>
2026-05-10 22:39:55 +0530

    started foodie lld

---
+++ foodie_lld/models/Restaurant.py
@@ -0,0 +1,34 @@
+# models/Restaurant.py
+
+from models.MenuItem import MenuItem
+
+class Restaurant:
+
+    next_restaurant_id = 0
+
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+        Restaurant.next_restaurant_id += 1
+        self.restaurant_id = Restaurant.next_restaurant_id
+
+        self.menu = []
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_location(self):
+        return self.location
+
+    def set_location(self, location):
+        self.location = location
+
+    def add_menu_item(self, item):
+        self.menu.append(item)
+
+    def get_menu(self):
+        return self.menu
